f60a2ae3e1d05df3b537b5bda706671d38e0939c
sizz <ssasun94@gmail.com>
2020-10-05 13:59:48 +0900

    update: https

--- newVersion/front/nuxt.config.js
+++ newVersion/front/nuxt.config.js
@@ -49,7 +49,7 @@ module.exports = {
     axios: {
         browserBaseURL: process.env.NODE_ENV === 'production' ? process.env.APIURL : 'http://localhost:3086/api',
         baseURL: process.env.NODE_ENV === 'production' ? process.env.APIURL : 'http://localhost:3086/api',
-        https: false,
+        https: true,
     },
     moment: {
         locales: ['ko'],
